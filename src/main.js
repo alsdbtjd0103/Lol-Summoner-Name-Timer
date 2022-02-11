490a8a779be7d42a1f9a1871ccdb863327f9b02a
ysyss <alsdbtjd0103@naver.com>
2022-02-11 19:44:21 +0900

    기본적ㅇ

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const searchBtn=document.querySelector("#searchBtn")
+function searchInformation(){
+    const name=document.querySelector("#input_name").value;
+    const region=document.querySelector("#region").value;
+    
+}
+function calculateTime(level,lastGame){
+
+}
+searchBtn.addEventListener("submit",searchInformation);
